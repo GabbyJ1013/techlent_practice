6726d51bf88cef67977114b1e03c9eb7df5bf9f0
gabriellajg <7454640+gabriellajg@users.noreply.github.com>
2022-03-19 00:29:41 -0500

    calcuator

--- app/main.py
+++ app/main.py
@@ -1,5 +1,31 @@
 from src.srcsub1.calculator import Calculator
+from flask import Flask, render_template, request
 
 cal = Calculator() # Realization
+app = Flask(__name__)
 
-print(cal.add(1,2))
+@app.route("/", methods = ["GET", "POST"]) # get: get info from backend; POST: stream info from frontend
+def index():
+    result = ""
+    if request.method == "POST":
+        num1 = request.form["num1"]
+        num2 = request.form["num2"]
+        operator = request.form["operator"]
+        if operator == "+":
+            result = cal.add(float(num1),float(num2))
+        elif operator == "-":
+            result = float(num1)-float(num2)
+        elif operator == "*":
+            result = float(num1)*float(num2)
+        else:
+            result = float(num1)/float(num2)
+
+    return render_template('index.html', result=result)
+
+@app.route('/health')  # show the contents on the subpage called "health"
+def health():
+    return "I am healthy"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1",port=5000)
+
